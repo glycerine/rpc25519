133b1caea5ab815000b5c75c00ae9fb56d0897c6
Jason E. Aten <j.e.aten@gmail.com>
2024-10-15 17:18:07 -0500

    atg. on the TCP testRPC port 006

--- cli_test.go
+++ cli_test.go
@@ -40,8 +40,30 @@ func Test006_RoundTrip_Using_NetRPC_API_TCP(t *testing.T) {
 		defer client.Close()
 
 		// net/rpc API on client, ported from net_server_test.go
-		var args *Args
-		var reply *Reply
+		//var args *Args
+		//var reply *Reply
+
+		// Synchronous calls
+		args := &Args{7, 8}
+		reply := new(Reply)
+		err = client.Call("Arith.Add", args, reply)
+		if err != nil {
+			t.Errorf("Add: expected no error but got string %q", err.Error())
+		}
+		if reply.C != args.A+args.B {
+			t.Errorf("Add: expected %d got %d", reply.C, args.A+args.B)
+		}
+
+		// Methods exported from unexported embedded structs
+		args = &Args{7, 0}
+		reply = new(Reply)
+		err = client.Call("Embed.Exported", args, reply)
+		if err != nil {
+			t.Errorf("Add: expected no error but got string %q", err.Error())
+		}
+		if reply.C != args.A+args.B {
+			t.Errorf("Add: expected %d got %d", reply.C, args.A+args.B)
+		}
 
 		// Nonexistent method
 		args = &Args{7, 0}
@@ -54,29 +76,14 @@ func Test006_RoundTrip_Using_NetRPC_API_TCP(t *testing.T) {
 			t.Errorf("BadOperation: expected can't find method error; got %q", err)
 		}
 
-		return
-
-		// Synchronous calls
+		// Unknown service
 		args = &Args{7, 8}
 		reply = new(Reply)
-		err = client.Call("Arith.Add", args, reply)
-		if err != nil {
-			t.Errorf("Add: expected no error but got string %q", err.Error())
-		}
-		if reply.C != args.A+args.B {
-			t.Errorf("Add: expected %d got %d", reply.C, args.A+args.B)
-		}
-		vv("Arith.Add got: %v + %v = %v", args.A, args.B, reply.C)
-
-		// Methods exported from unexported embedded structs
-		args = &Args{7, 0}
-		reply = new(Reply)
-		err = client.Call("Embed.Exported", args, reply)
-		if err != nil {
-			t.Errorf("Add: expected no error but got string %q", err.Error())
-		}
-		if reply.C != args.A+args.B {
-			t.Errorf("Add: expected %d got %d", reply.C, args.A+args.B)
+		err = client.Call("Arith.Unknown", args, reply)
+		if err == nil {
+			t.Error("expected error calling unknown service")
+		} else if !strings.Contains(err.Error(), "method") {
+			t.Error("expected error about method; got", err)
 		}
 
 		// Out of order.
@@ -100,6 +107,70 @@ func Test006_RoundTrip_Using_NetRPC_API_TCP(t *testing.T) {
 		}
 		if mulReply.C != args.A*args.B {
 			t.Errorf("Mul: expected %d got %d", mulReply.C, args.A*args.B)
+		}
+
+		// Error test
+		args = &Args{7, 0}
+		reply = new(Reply)
+		err = client.Call("Arith.Div", args, reply)
+		// expect an error: zero divide
+		if err == nil {
+			t.Error("Div: expected error")
+		} else if err.Error() != "divide by zero" {
+			t.Error("Div: expected divide by zero error; got", err)
+		}
+
+		// Bad type.
+		reply = new(Reply)
+		err = client.Call("Arith.Add", reply, reply) // args, reply would be the correct thing to use
+		if err == nil {
+			t.Error("expected error calling Arith.Add with wrong arg type")
+		} else if !strings.Contains(err.Error(), "type") {
+			t.Error("expected error about type; got", err)
+		}
+
+		// Non-struct argument
+		const Val = 12345
+		str := fmt.Sprint(Val)
+		reply = new(Reply)
+		err = client.Call("Arith.Scan", &str, reply)
+		if err != nil {
+			t.Errorf("Scan: expected no error but got string %q", err.Error())
+		} else if reply.C != Val {
+			t.Errorf("Scan: expected %d got %d", Val, reply.C)
+		}
+
+		// Non-struct reply
+		args = &Args{27, 35}
+		str = ""
+		err = client.Call("Arith.String", args, &str)
+		if err != nil {
+			t.Errorf("String: expected no error but got string %q", err.Error())
+		}
+		expect := fmt.Sprintf("%d+%d=%d", args.A, args.B, args.A+args.B)
+		if str != expect {
+			t.Errorf("String: expected %s got %s", expect, str)
+		}
+
+		args = &Args{7, 8}
+		reply = new(Reply)
+		err = client.Call("Arith.Mul", args, reply)
+		if err != nil {
+			t.Errorf("Mul: expected no error but got string %q", err.Error())
+		}
+		if reply.C != args.A*args.B {
+			t.Errorf("Mul: expected %d got %d", reply.C, args.A*args.B)
+		}
+
+		// ServiceName contain "." character
+		args = &Args{7, 8}
+		reply = new(Reply)
+		err = client.Call("net.rpc.Arith.Add", args, reply)
+		if err != nil {
+			t.Errorf("Add: expected no error but got string %q", err.Error())
+		}
+		if reply.C != args.A+args.B {
+			t.Errorf("Add: expected %d got %d", reply.C, args.A+args.B)
 		}
 
 	})
