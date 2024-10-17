371664333e833487cfce859f3ee5e409aae077f9
Jason E. Aten <j.e.aten@gmail.com>
2024-10-17 14:57:25 -0500

    export goq needed NetConnWrapper

--- quic_client.go
+++ quic_client.go
@@ -177,7 +177,7 @@ func (c *Client) RunQUIC(localHostPort, quicServerAddr string, tlsConfig *tls.Co
 	}
 	defer stream.Close()
 
-	wrap := &netConnWrapper{Stream: stream, Connection: conn}
+	wrap := &NetConnWrapper{Stream: stream, Connection: conn}
 
 	//vv("client: local = '%v'", local(wrap))
 	//vv("client: remote = '%v'", remote(wrap))
@@ -186,7 +186,7 @@ func (c *Client) RunQUIC(localHostPort, quicServerAddr string, tlsConfig *tls.Co
 	c.RunReadLoop(wrap)
 }
 
-type netConnWrapper struct {
+type NetConnWrapper struct {
 	quic.Stream
 	quic.Connection
 }
--- quic_server.go
+++ quic_server.go
@@ -237,7 +237,7 @@ type quicRWPair struct {
 
 func (s *Server) newQUIC_RWPair(stream quic.Stream, conn quic.Connection) *quicRWPair {
 
-	wrap := &netConnWrapper{Stream: stream, Connection: conn}
+	wrap := &NetConnWrapper{Stream: stream, Connection: conn}
 
 	//vv("Server new quic pair: local = '%v'", local(wrap))
 	//vv("Server new quic pair: remote = '%v'", remote(wrap))
@@ -310,7 +310,7 @@ func (s *quicRWPair) runRecvLoop(stream quic.Stream, conn quic.Connection) {
 	w := newBlabber(s.Server.cfg.preSharedKey, stream, s.Server.cfg.encryptPSK, maxMessage)
 	//w := newWorkspace(maxMessage)
 
-	wrap := &netConnWrapper{Stream: stream, Connection: conn}
+	wrap := &NetConnWrapper{Stream: stream, Connection: conn}
 
 	var callme1 OneWayFunc
 	var callme2 TwoWayFunc
