3d42b73999743c0503ba2aadcf8ebc47e15c5001
Jason E. Aten <j.e.aten@gmail.com>
2024-10-19 15:14:56 -0500

    atg. log if pre-shared key used

--- cli.go
+++ cli.go
@@ -556,7 +556,7 @@ func (cfg *Config) checkPreSharedKey(name string) {
 		}
 		copy(cfg.preSharedKey[:], by)
 		cfg.encryptPSK = true
-		//alwaysPrintf("activated pre-shared-key on '%v' from cfg.PreSharedKeyPath='%v'", name, cfg.PreSharedKeyPath)
+		alwaysPrintf("activated pre-shared-key on '%v' from cfg.PreSharedKeyPath='%v'", name, cfg.PreSharedKeyPath)
 	}
 }
 
