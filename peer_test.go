788d0cd1f9261a63145a26508ee52bc669d20bf6
Jason E. Aten, Ph.D. <jason@devnull>
2025-01-16 12:42:19 +0000

    wip peer_test test junk reusable setup

--- peer_test.go
+++ peer_test.go
@@ -11,6 +11,63 @@ import (
 	cv "github.com/glycerine/goconvey/convey"
 )
 
+type testJunk struct {
+	name string
+	cfg  *Config
+	srv  *Server
+	cli  *Client
+
+	srvSync *syncer
+	cliSync *syncer
+}
+
+func (j *testJunk) cleanup() {
+	j.cli.Close()
+	j.srv.Close()
+}
+
+func newTestJunk(name string) (j *testJunk) {
+
+	j = &testJunk{
+		name: name,
+	}
+
+	cfg := NewConfig()
+	cfg.TCPonly_no_TLS = true
+
+	cfg.ServerAddr = "127.0.0.1:0"
+	srv := NewServer("srv_"+name, cfg)
+
+	serverAddr, err := srv.Start()
+	panicOn(err)
+	//defer srv.Close()
+
+	cfg.ClientDialToHostPort = serverAddr.String()
+	cli, err := NewClient("cli_"+name, cfg)
+	panicOn(err)
+	err = cli.Start()
+	panicOn(err)
+	//defer cli.Close()
+
+	srvSync := newSyncer("srvSync_" + name)
+
+	err = srv.PeerAPI.RegisterPeerServiceFunc("srvSync_"+name, srvSync.Start)
+	panicOn(err)
+
+	cliSync := newSyncer("cliSync_" + name)
+	err = cli.PeerAPI.RegisterPeerServiceFunc("cliSync_"+name, cliSync.Start)
+	panicOn(err)
+
+	j.cli = cli
+	j.srv = srv
+	j.cfg = cfg
+
+	j.cliSync = cliSync
+	j.srvSync = srvSync
+
+	return j
+}
+
 func Test404_verify_peer_operations(t *testing.T) {
 
 	cv.Convey("verify all the basic peer operations are functional", t, func() {
@@ -41,6 +98,11 @@ func Test404_verify_peer_operations(t *testing.T) {
 		err = srv.PeerAPI.RegisterPeerServiceFunc("srvSync", srvSync.Start)
 		panicOn(err)
 
+		// minimal base setup done!
+
+		// does cancel of local on server work?
+		// does cancel of local on client work?
+
 		cliSync := newSyncer("cliSync")
 
 		cli.PeerAPI.RegisterPeerServiceFunc("cliSync", cliSync.Start)
@@ -68,4 +130,14 @@ func Test404_verify_peer_operations(t *testing.T) {
 		//local service -> all local ckt cancel
 		// start here [ ] local ckt -> remote ckt
 	})
+}
+
+func Test405_user_can_cancel_local_service_with_context(t *testing.T) {
+
+	cv.Convey("user code calling cancel on a running local peer service should stop it", t, func() {
+		j := newTestJunk("local_ctx_cancel_test405")
+		defer j.cleanup()
+
+	})
+
 }
