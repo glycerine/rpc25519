b695c7a3f3d5d45c1808b4a9aa27481519ffd5a7
Jason E. Aten, Ph.D. <jason@devnull>
2025-01-17 16:13:23 +0000

    atg. document Mutexmap

--- mutmap.go
+++ mutmap.go
@@ -4,17 +4,21 @@ import (
 	"sync"
 )
 
+// Mutexmap is simply a generic map protected by a sync.RWMutex.
+// This provides fine-grained locking for goroutine safety.
 type Mutexmap[K comparable, V any] struct {
 	mut sync.RWMutex
 	m   map[K]V
 }
 
+// NewMutexmap creates a new mutex-protected map.
 func NewMutexmap[K comparable, V any]() *Mutexmap[K, V] {
 	return &Mutexmap[K, V]{
 		m: make(map[K]V),
 	}
 }
 
+// Get returns the value val for key.
 func (m *Mutexmap[K, V]) Get(key K) (val V, ok bool) {
 	m.mut.RLock()
 	val, ok = m.m[key]
@@ -22,6 +26,7 @@ func (m *Mutexmap[K, V]) Get(key K) (val V, ok bool) {
 	return
 }
 
+// GetValSlice returns all the values in the map in slc.
 func (m *Mutexmap[K, V]) GetValSlice() (slc []V) {
 	m.mut.RLock()
 	for _, v := range m.m {
@@ -31,19 +36,22 @@ func (m *Mutexmap[K, V]) GetValSlice() (slc []V) {
 	return
 }
 
+// Set a single key to value val.
 func (m *Mutexmap[K, V]) Set(key K, val V) {
 	m.mut.Lock()
 	m.m[key] = val
 	m.mut.Unlock()
 }
 
+// Del deletes key from the map.
 func (m *Mutexmap[K, V]) Del(key K) {
 	m.mut.Lock()
 	delete(m.m, key)
 	m.mut.Unlock()
 }
 
-// n gives the count of items left in map after deleting key.
+// GetValNDel returns the val for key, and deletes it.
+// The returned n gives the count of items left in map after deleting key.
 func (m *Mutexmap[K, V]) GetValNDel(key K) (val V, n int, ok bool) {
 	m.mut.Lock()
 	val, ok = m.m[key]
@@ -55,7 +63,7 @@ func (m *Mutexmap[K, V]) GetValNDel(key K) (val V, n int, ok bool) {
 	return
 }
 
-// getN returns the number of keys in the map.
+// GetN returns the number of keys in the map.
 func (m *Mutexmap[K, V]) GetN() (n int) {
 	m.mut.RLock()
 	n = len(m.m)
@@ -63,6 +71,7 @@ func (m *Mutexmap[K, V]) GetN() (n int) {
 	return
 }
 
+// Clear delets all keys from the map.
 func (m *Mutexmap[K, V]) Clear() {
 	m.mut.Lock()
 	clear(m.m)
