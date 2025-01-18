774bd7d2384520978110508ae54ee83e0117e5d8
Jason E. Aten, Ph.D. <jason@devnull>
2025-01-18 14:14:05 +0000

    docs++

--- cktdoc.go
+++ cktdoc.go
@@ -2,7 +2,8 @@
 Peer/Circuit/Fragment Design overview
 
 This is an alternative API to the Message and net/rpc APIs
-provided by the rpc25519 package.
+provided by the rpc25519 package. It is a layer
+on top of the Message API.
 
 Motivated by filesystem syncing, we envision a system that can
 both stream efficiently and utilize the same code
@@ -35,7 +36,8 @@ for instance. Multiple large files being
 scanned and their diffs streamed at once,
 in parallel, becomes practical.
 
-By using lightweight goroutines and channels,
+Go's features enables such a design. By using
+lightweight goroutines and channels,
 circuit persistence is inexpensive and
 supports any number of data streams with markedly
 different lifetimes and update rates,
@@ -52,6 +54,11 @@ This is not available in a typical RPC package.
 
 Peer/Circuit/Fragment API essentials (utility methods omitted for compactness)
 
+The user implements and registers a PeerServiceFunc callback
+with the Client and/or Server. There are multiple examples of
+this in the test files. Once registered, from within your
+PeerServiceFunc implementation:
+
 A) To establish circuits with new peers, use
 
  1. NewCircuitToPeerURL() for initiating a new circuit to a new peer.
@@ -65,7 +72,7 @@ C) To communicate over a Circuit:
  1. get regular messages (called Fragments) from <-Circuit.Reads
  2. get error messages from <-Circuit.Errors
  3. send messages with Circuit.SendOneWay(). It never blocks.
- 4. Close() the circuit and the peer's ctx will be cancelled.
+ 4. Close() the circuit and both the local and remote Circuit.Context will be cancelled.
 
 // Circuit has other fields, but this is the essential interface:
 
@@ -77,14 +84,16 @@ C) To communicate over a Circuit:
 	    NewCircuit() (ckt *Circuit, ctx context.Context, err error) // make 2nd, 3rd.
 	}
 
+// Your PeerServiceFunc gets a pointer to its *LocalPeer as its first argument.
 // LocalPeer is actually a struct, but you can think of it as this interface:
 
 	type LocalPeer interface {
 		NewCircuitToPeerURL(peerURL string, frag *Fragment,
-	         errWriteDur *time.Duration) (ckt *Circuit, ctx context.Context, err error)
+	         errWriteDur time.Duration) (ckt *Circuit, ctx context.Context, err error)
 	}
 
-// Users write PeerServiceFunc callbacks to create peers.
+// As above, users write PeerServiceFunc callbacks to create peers.
+// This is the full type:
 
 	type PeerServiceFunc func(myPeer *LocalPeer, ctx0 context.Context, newCircuitCh <-chan *Circuit) error
 
@@ -127,8 +136,8 @@ D)   boostrapping:
 	       peerServiceName string) (lp *LocalPeer, err error)
 
 	   Starting a remote peer must also specify the host:port remoteAddr
-	   of the remote client/server. The user can call the RemoteAddr() and
-	   LocalAddr() methods on the Client/Server to obtain these.
+	   of the remote client/server. The user can call the RemoteAddr()
+	   method on the Client/Server to obtain this.
 
 	       PeerAPI.StartRemotePeer(
 	                    ctx context.Context,
@@ -137,7 +146,7 @@ D)   boostrapping:
 	               waitUpTo time.Duration,
 	                           ) (remotePeerURL, remotePeerID string, err error)
 
-	    The returned URLs can be used in myPeer.NewCircuitToPeerURL() calls
-	    inside the PeerServiceFunc.
+	    The returned URL can be used in LocalPeer.NewCircuitToPeerURL() calls,
+	    for instance on myPeer inside the PeerServiceFunc callback.
 */
 package rpc25519
