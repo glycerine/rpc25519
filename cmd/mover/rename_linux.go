d5c2e164e337f2d37a1c018f50525285fdaea79e
Jason E. Aten <j.e.aten@gmail.com>
2025-01-27 14:12:36 -0600

    linux also gets hardlinks

--- cmd/mover/rename_linux.go
+++ cmd/mover/rename_linux.go
@@ -3,6 +3,11 @@
 
 package main
 
+import (
+	"os"
+	"syscall"
+)
+
 /*
 #define _GNU_SOURCE
 #include <fcntl.h>
@@ -13,6 +18,10 @@ package main
 
 int my_renameat2(const char *oldpath, const char *newpath, unsigned int flags) {
     return syscall(SYS_renameat2, AT_FDCWD, oldpath, AT_FDCWD, newpath, flags);
+}
+
+int _linkat(int olddirfd, const char *oldpath, int newdirfd, const char *newpath, int flags) {
+    return linkat(olddirfd, oldpath, newdirfd, newpath, flags);
 }
 */
 import "C"
@@ -30,4 +39,18 @@ func atomicDirSwap(oldpath, newpath string) error {
 		return fmt.Errorf("renameat2 failed: %v", err)
 	}
 	return nil
+}
+
+func makeHardlink(origFilePath, newFilePath string) {
+	origFilePathC := C.CString(origFilePath)
+	newFilePathC := C.CString(newFilePath)
+
+	result := C._linkat(C.AT_FDCWD, origFilePathC, C.AT_FDCWD, newFilePathC, 0)
+	fmt.Println(result)
+	if result < 0 {
+		fmt.Print("errno: ")
+		fmt.Println(syscall.Errno(-result))
+		fmt.Println("Error creating hardlink")
+		os.Exit(1)
+	}
 }
