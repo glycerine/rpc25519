9164fa0d5be0ade0d54aeb173c57d6b8702351e1
Jason E. Aten, Ph.D. <jason@devnull>
2025-01-27 18:51:47 +0000

    linux tries to use renameat2

--- cmd/mover/rename_linux.go
+++ cmd/mover/rename_linux.go
@@ -8,33 +8,16 @@ import (
 	"syscall"
 )
 
-// Linux-specific constants
 const (
 	_RENAME_EXCHANGE = 0x2
 )
 
-func atomicDirReplace(oldpath, newpath string) error {
-	// Get file descriptors for the parent directories
-	oldDir, oldName := filepath.Split(oldpath)
-	newDir, newName := filepath.Split(newpath)
+//go:noescape
+func renameat2(olddirfd int, oldpath string, newdirfd int, newpath string, flags uint) (err syscall.Errno)
 
-	oldFd, err := syscall.Open(oldDir, syscall.O_DIRECTORY, 0)
-	if err != nil {
-		return fmt.Errorf("failed to open old directory: %w", err)
+func atomicDirSwap(oldpath, newpath string) error {
+	if err := renameat2(-100, oldpath, -100, newpath, _RENAME_EXCHANGE); err != 0 {
+		return fmt.Errorf("renameat2 failed: %s", err)
 	}
-	defer syscall.Close(oldFd)
-
-	newFd, err := syscall.Open(newDir, syscall.O_DIRECTORY, 0)
-	if err != nil {
-		return fmt.Errorf("failed to open new directory: %w", err)
-	}
-	defer syscall.Close(newFd)
-
-	// Perform the atomic swap
-	err = syscall.Renameat2(oldFd, oldName, newFd, newName, _RENAME_EXCHANGE)
-	if err != nil {
-		return fmt.Errorf("renameat2 failed: %w", err)
-	}
-
 	return nil
 }
---
+++ cmd/mover/rename_linux_amd64.s
@@ -0,0 +1,17 @@
+// +build linux
+// +build amd64
+
+#include "textflag.h"
+
+// func renameat2(olddirfd int, oldpath string, newdirfd int, newpath string, flags uint) (err syscall.Errno)
+TEXT ·renameat2(SB),NOSPLIT,$0
+    MOVQ    olddirfd+0(FP), DI     // first argument: old dir fd
+    MOVQ    oldpath+8(FP), SI      // second argument: old path string pointer
+    MOVQ    newdirfd+32(FP), DX    // third argument: new dir fd
+    MOVQ    newpath+40(FP), R10    // fourth argument: new path string pointer
+    MOVQ    flags+64(FP), R8       // fifth argument: flags
+    MOVQ    $316, AX               // syscall number for renameat2
+    SYSCALL
+    NEGQ    AX                     // negate return for error
+    MOVQ    AX, err+72(FP)         // store error code
+    RET
