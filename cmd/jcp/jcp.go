6644e1f825860ac7b3fa4d837a24000896bf0ea8
Jason E. Aten, Ph.D. <jason@devnull>
2025-02-03 19:40:55 +0000

    quiet

--- cmd/jcp/jcp.go
+++ cmd/jcp/jcp.go
@@ -118,7 +118,7 @@ func main() {
 		if len(splt2) <= 1 {
 			//fmt.Fprintf(os.Stderr, "jcp error: neither source nor destination had ':' in it. Which is the remote?\n")
 			//os.Exit(1)
-			fmt.Printf("no ':' in src/target: starting local rsync server to receive files...\n")
+			//fmt.Printf("no ':' in src/target: starting local rsync server to receive files...\n")
 			cfg.ServerAddr = "127.0.0.1:0"
 			srv := rpc.NewServer("srv_rsync_jcp", cfg)
 			serverAddr, err := srv.Start()
@@ -185,9 +185,9 @@ func main() {
 			panic(fmt.Sprintf("problem: takerPath cannot be an existing dir: '%v'", takerPath))
 		}
 	*/
-	vv("dest = '%v'", dest)
-	vv("takerPath = '%v' exists=%v; isDir=%v", takerPath, takerExistsLocal, takerIsDir)
-	vv("giverPath = '%v' exists=%v; isDir=%v", giverPath, giverExistsLocal, giverIsDir)
+	//vv("dest = '%v'", dest)
+	//vv("takerPath = '%v' exists=%v; isDir=%v", takerPath, takerExistsLocal, takerIsDir)
+	//vv("giverPath = '%v' exists=%v; isDir=%v", giverPath, giverExistsLocal, giverIsDir)
 
 	var giverStartsEmpty bool
 	var takerStartsEmpty bool
@@ -297,9 +297,9 @@ func main() {
 	defer canc()
 	_ = ctx
 
-	vv("jcp about to send on reqs chan")
+	//vv("jcp about to send on reqs chan")
 	reqs <- req
-	vv("jcp sent on reqs: requested to rsync to '%v' from %v:%v", takerPath, dest, giverPath)
+	//vv("jcp sent on reqs: requested to rsync to '%v' from %v:%v", takerPath, dest, giverPath)
 	<-req.Done.Chan
 
 	if req.Errs != "" {
@@ -307,7 +307,7 @@ func main() {
 		os.Exit(1)
 	}
 	//vv("req (%p) had no req.Errs; empty string ('%v'): '%#v'", req, req.Errs, req)
-	vv("all good. elapsed time: %v", time.Since(t0))
+	//vv("all good. elapsed time: %v", time.Since(t0))
 	switch {
 	case req.SizeModTimeMatch:
 		vv("jcp rsync done: good size and mod time match for '%v'", takerPath)
