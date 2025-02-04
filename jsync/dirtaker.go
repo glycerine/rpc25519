a126702bf266bc769c11a76d5b21e2da6720cc3d
Jason E. Aten, Ph.D. <jason@devnull>
2025-02-04 14:56:12 +0000

    turn back on old dir delete

--- jsync/dirtaker.go
+++ jsync/dirtaker.go
@@ -175,8 +175,8 @@ func (s *SyncService) DirTaker(ctx0 context.Context, ckt *rpc.Circuit, myPeer *r
 				if old != "" {
 					// We have hard linked all the unchanged files into the new.
 					// Now delete the old version (hard link count -> 1).
-					vv("TODO debug actually remove old dir: '%v'", old)
-					//panicOn(os.RemoveAll(old))
+					//vv("TODO debug actually remove old dir: '%v'", old)
+					panicOn(os.RemoveAll(old))
 				}
 				// and set the mod time
 				if !reqDir.GiverDirModTime.IsZero() {
