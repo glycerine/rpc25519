57b9e5544593d7a139846658dcf78c0a820fc80b
Jason E. Aten <j.e.aten@gmail.com>
2025-02-09 09:33:24 -0600

    switch defaut to pla

--- jsync/service.go
+++ jsync/service.go
@@ -40,10 +40,10 @@ func SetParallelChunking(set bool) (old bool) {
 // the fewest bytes need to be updated.
 
 // 8-12 sec on linux Ub lan sync.
-//var Default_CDC jcdc.CDCAlgo = jcdc.FastCDC_PlakarAlgo // 22.080s, mac. 27.6MB.
+var Default_CDC jcdc.CDCAlgo = jcdc.FastCDC_PlakarAlgo // 22.080s, mac. 27.6MB.
 
 // 13sec linux.
-var Default_CDC jcdc.CDCAlgo = jcdc.FastCDC_StadiaAlgo // 28.3s, mac. 1.4MB.
+//var Default_CDC jcdc.CDCAlgo = jcdc.FastCDC_StadiaAlgo // 28.3s, mac. 1.4MB.
 
 // UltaCDC: very bad dedup, 1GB instead of 1MB. slower.
 //var Default_CDC jcdc.CDCAlgo = jcdc.UltraCDC_Algo
