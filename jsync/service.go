b4a44fe3c151d2e1df1e4fe14c57ae784e45dee0
Jason E. Aten <j.e.aten@gmail.com>
2025-02-09 08:17:06 -0600

    p fastcdc is looking faster

--- jsync/service.go
+++ jsync/service.go
@@ -28,9 +28,11 @@ import (
 //
 // In our benchmarks, FastCDC_PlakarAlgo results in
 // the fewest bytes need to be updated.
-var Default_CDC jcdc.CDCAlgo = jcdc.FastCDC_PlakarAlgo // 22.080s, mac. 27.6MB.
 
-//var Default_CDC jcdc.CDCAlgo = jcdc.FastCDC_StadiaAlgo // 28.3s, mac. 1.4MB.
+// 8-14 sec on linux Ub lan sync.
+//var Default_CDC jcdc.CDCAlgo = jcdc.FastCDC_PlakarAlgo // 22.080s, mac. 27.6MB.
+
+var Default_CDC jcdc.CDCAlgo = jcdc.FastCDC_StadiaAlgo // 28.3s, mac. 1.4MB.
 
 // UltaCDC: very bad dedup, 1GB instead of 1MB. slower.
 //var Default_CDC jcdc.CDCAlgo = jcdc.UltraCDC_Algo
