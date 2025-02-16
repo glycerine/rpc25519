76affcf0a9b6da266763f5eb0f59c70915a2fc38
Jason E. Aten, Ph.D. <jason@devnull>
2025-02-16 15:03:06 +0000

    quieter

--- jsync/taker.go
+++ jsync/taker.go
@@ -341,7 +341,7 @@ takerForSelectLoop:
 				panicOn(err)
 				bt.bread += len(frag.Payload)
 
-				vv("%v: (ckt %v) (Taker) sees OpRsync_ToTakerMetaUpdateAtLeast. updating mode/modTime on '%v' to '%v'", name, ckt.Name, syncReq.TakerPath, precis.ModTime)
+				//vv("%v: (ckt %v) (Taker) sees OpRsync_ToTakerMetaUpdateAtLeast. updating mode/modTime on '%v' to '%v'", name, ckt.Name, syncReq.TakerPath, precis.ModTime)
 
 				if localPathToWrite != localPathToRead {
 					//vv("hard linking 6 '%v' <- '%v'",localPathToRead, localPathToWrite)
