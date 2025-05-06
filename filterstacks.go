8e63d2a783b196cae5abc97035e37dccfbf28ea2
Jason E. Aten, Ph.D. <jason@devnull>
2025-05-06 17:15:52 +0100

    filterstacks shows timestamp

--- filterstacks.go
+++ filterstacks.go
@@ -9,6 +9,7 @@ import (
 	"runtime"
 	"strings"
 	"syscall"
+	"time"
 )
 
 var sigQuitCh chan os.Signal
@@ -41,7 +42,9 @@ func init() {
 				}
 			}
 
-			os.Stderr.Write([]byte("\nSIGQUIT: quit after filtering.\nfilterstacks.go filtered stacks:\n\n"))
+			ts := "\n" + time.Now().In(gtz).Format("2006-01-02 15:04:05.999 -0700 MST")
+			os.Stderr.Write([]byte(ts))
+			os.Stderr.Write([]byte("SIGQUIT: quit after filtering.\nfilterstacks.go filtered stacks:\n\n"))
 			os.Stderr.Write([]byte(strings.Join(filtered, "\n\n")))
 			os.Exit(1)
 		}
