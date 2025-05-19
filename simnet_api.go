4358a27f78851384b201d663684a4f93a960970b
Jason E. Aten, Ph.D. <jason@devnull>
2025-05-20 00:16:50 +0100

    partial move to simnet_api

---
+++ simnet_api.go
@@ -0,0 +1,221 @@
+package rpc25519
+
+import (
+	"fmt"
+	"time"
+)
+
+// ===========================================
+// external API below, safe for client use.
+// ===========================================
+
+// DropDeafSpec specifies a network/netcard fault
+// with a given probability.
+type DropDeafSpec struct {
+
+	// false UpdateDeafReads means no change to deafRead probability.
+	UpdateDeafReads bool
+
+	// false UpdateDropSends means no change to dropSend probability.
+	UpdateDropSends bool
+
+	// probability of ignoring (being deaf) to a read.
+	// 0 => never be deaf to a read (healthy).
+	// 1 => ignore all reads (dead).
+	DeafReadsNewProb float64
+
+	// probability of dropping a send.
+	// 0 => never drop a send (healthy).
+	// 1 => always drop a send (dead).
+	DropSendsNewProb float64
+}
+
+type circuitFault struct {
+	originName string
+	targetName string
+	DropDeafSpec
+	sn      int64
+	proceed chan struct{}
+	err     error
+}
+
+func newCircuitFault(originName, targetName string, dd DropDeafSpec) *circuitFault {
+	return &circuitFault{
+		originName:   originName,
+		targetName:   targetName,
+		DropDeafSpec: dd,
+		sn:           simnetNextMopSn(),
+		proceed:      make(chan struct{}),
+	}
+}
+
+type hostFault struct {
+	hostName string
+	DropDeafSpec
+	sn      int64
+	proceed chan struct{}
+	err     error
+}
+
+func newHostFault(hostName string, dd DropDeafSpec) *hostFault {
+	return &hostFault{
+		hostName:     hostName,
+		DropDeafSpec: dd,
+		sn:           simnetNextMopSn(),
+		proceed:      make(chan struct{}),
+	}
+}
+
+type circuitRepair struct {
+	originName string
+	targetName string
+
+	justOriginHealed bool
+	unIsolate        bool
+	powerOnIfOff     bool
+	sn               int64
+	proceed          chan struct{}
+	err              error
+}
+
+func (s *simnet) newCircuitRepair(originName, targetName string, unIsolate, powerOnIfOff, justOrigin bool) *circuitRepair {
+	return &circuitRepair{
+		justOriginHealed: justOrigin,
+		originName:       originName,
+		targetName:       targetName,
+		unIsolate:        unIsolate,
+		powerOnIfOff:     powerOnIfOff,
+		sn:               simnetNextMopSn(),
+		proceed:          make(chan struct{}),
+	}
+}
+
+type hostRepair struct {
+	hostName     string
+	powerOnIfOff bool
+	unIsolate    bool
+	allHosts     bool
+	sn           int64
+	proceed      chan struct{}
+	err          error
+}
+
+func (s *simnet) newHostRepair(hostName string, unIsolate, powerOnIfOff, allHosts bool) *hostRepair {
+	m := &hostRepair{
+		hostName:     hostName,
+		powerOnIfOff: powerOnIfOff,
+		unIsolate:    unIsolate,
+		allHosts:     allHosts,
+		sn:           simnetNextMopSn(),
+		proceed:      make(chan struct{}),
+	}
+	return m
+}
+
+// empty string target means all possible targets
+func (s *simnet) CircuitFault(origin, target string, dd DropDeafSpec) (err error) {
+
+	fault := newCircuitFault(origin, target, dd)
+
+	select {
+	case s.injectCircuitFaultCh <- fault:
+		//vv("sent DeafToReads fault on injectFaultCh; about to wait on proceed")
+	case <-s.halt.ReqStop.Chan:
+		return
+	}
+	select {
+	case <-fault.proceed:
+		err = fault.err
+		if target == "" {
+			target = "(any and all)"
+		}
+		//vv("server '%v' CircuitFault from '%v'; err = '%v'", origin, target, err)
+	case <-s.halt.ReqStop.Chan:
+		return
+	}
+	return
+}
+
+func (s *simnet) HostFault(hostName string, dd DropDeafSpec) (err error) {
+
+	fault := newHostFault(hostName, dd)
+
+	select {
+	case s.injectHostFaultCh <- fault:
+		//vv("sent fault on injectHostFaultCh; about to wait on proceed")
+	case <-s.halt.ReqStop.Chan:
+		return
+	}
+	select {
+	case <-fault.proceed:
+		err = fault.err
+		//vv("server '%v' hostFault from '%v'; dd='%v'; err = '%v'", hostName, dd, err)
+	case <-s.halt.ReqStop.Chan:
+		return
+	}
+	return
+}
+
+// RepairCircuit restores the local circuit to
+// full working order. It undoes the effects of
+// prior deafDrop actions, if any. It does not
+// change an isolated simnode's isolation unless unIsolate
+// is also true. See also RepairHost, AllHealthy.
+// .
+func (s *simnet) RepairCircuit(originName string, unIsolate bool, powerOnIfOff bool) (err error) {
+
+	targetName := "" // all corresponding targets
+	const justOrigin_NO = false
+	oneGood := s.newCircuitRepair(originName, targetName, unIsolate, powerOnIfOff, justOrigin_NO)
+
+	select {
+	case s.repairCircuitCh <- oneGood:
+		//vv("RepairCircuit sent oneGood on repairCircuitCh; about to wait on proceed")
+	case <-s.halt.ReqStop.Chan:
+		return
+	}
+	select {
+	case <-oneGood.proceed:
+		err = oneGood.err
+		//vv("RepairCircuit '%v' done. err = '%v'", originName, err)
+	case <-s.halt.ReqStop.Chan:
+		return
+	}
+	return
+}
+
+// RepairHost repairs all the circuits on the host.
+func (s *simnet) RepairHost(originName string, unIsolate bool, powerOnIfOff, allHosts bool) (err error) {
+	//vv("top of RepairHost, originName = '%v'; unIsolate=%v, powerOnIfOff=%v, allHosts=%v", originName, unIsolate, powerOnIfOff, allHosts)
+	//defer func() {
+	//vv("end of RepairHost('%v')", originName)
+	//}()
+
+	repair := s.newHostRepair(originName, unIsolate, powerOnIfOff, allHosts)
+
+	select {
+	case s.repairHostCh <- repair:
+		//vv("RepairHost sent repair on repairHostCh; about to wait on proceed")
+	case <-s.halt.ReqStop.Chan:
+		return
+	}
+	select {
+	case <-repair.proceed:
+		err = repair.err
+		//vv("RepairHost '%v' done. err = '%v'", originName, err)
+	case <-s.halt.ReqStop.Chan:
+		return
+	}
+	return
+}
+
+// AllHealthy heal all partitions, undo all faults, network wide.
+// All circuits are returned to HEALTHY status. Their powerOff status
+// is not updated unless powerOnIfOff is also true.
+// See also RepairSimnode for single simnode repair.
+// .
+func (s *simnet) AllHealthy(powerOnIfOff bool) (err error) {
+	vv("AllHealthy(powerOnIfOff=%v) called.", powerOnIfOff)
+
+	return s.RepairHost("", true, powerOnIfOff, true)
+}
