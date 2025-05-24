652d713d2fe75699a7faef99e180b4bb8683d4ba
Jason E. Aten, Ph.D. <jason@devnull>
2025-05-24 03:27:46 +0100

    dmap based on rbtree

--- dmap.go
+++ dmap.go
@@ -2,7 +2,8 @@ package rpc25519
 
 import (
 	"iter"
-	"sort"
+
+	rb "github.com/glycerine/rbtree"
 )
 
 type ided interface {
@@ -15,69 +16,70 @@ type ided interface {
 // sortable id() string which determintes
 // the range all() order.
 type dmap[K ided, V any] struct {
-	keys  []string
-	vals  []V
-	ideds []K
-	// lazy index, can be made on demand so our
-	// zero value is useful without an init.
-	idx map[string]bool
+	tree *rb.Tree
+	idx  map[string]rb.Iterator
 }
 
 func newDmap[K ided, V any]() *dmap[K, V] {
 	return &dmap[K, V]{
-		idx: make(map[string]bool),
+		idx: make(map[string]rb.Iterator),
+		tree: rb.NewTree(func(a, b rb.Item) int {
+			ak := a.(*ikv[K, V]).id
+			bk := b.(*ikv[K, V]).id
+			if ak < bk {
+				return -1
+			}
+			if ak > bk {
+				return 1
+			}
+			return 0
+		}),
 	}
 }
 
-func (s *dmap[K, V]) upsert(k K, val V) {
-	key := k.id()
-	if s.idx == nil {
-		s.idx = make(map[string]bool)
-	} else {
-		if s.idx[key] {
-			i := sort.Search(len(s.keys), func(i int) bool {
-				return key <= s.keys[i]
-			})
-			s.vals[i] = val // updated value for key
-			return
-		}
-	}
-	// not present already
-	s.idx[key] = true
+type ikv[K ided, V any] struct {
+	id  string // sorted order
+	key K
+	val V
+}
 
-	i := sort.Search(len(s.keys), func(i int) bool {
-		return key <= s.keys[i]
-	})
-	if i == len(s.keys) {
-		// key is larger than everything else
-		s.keys = append(s.keys, key)
-		s.vals = append(s.vals, val)
-		s.ideds = append(s.ideds, k)
+func (s *dmap[K, V]) upsert(key K, val V) {
+
+	id := key.id()
+
+	var it rb.Iterator
+	var ok bool
+	if s.idx == nil {
+		s.idx = make(map[string]rb.Iterator)
+	} else {
+		it, ok = s.idx[id]
+	}
+
+	if !ok {
+		// not yet in idx/tree, so add it.
+		item := &ikv[K, V]{id: id, key: key, val: val}
+		added, it2 := s.tree.InsertGetIt(item)
+		if !added {
+			panic("should have hit in s.idx, out of sync with tree")
+		}
+		s.idx[id] = it2
 		return
 	}
-	s.keys = append(s.keys[:i], append([]string{key}, s.keys[i:]...)...)
-	s.vals = append(s.vals[:i], append([]V{val}, s.vals[i:]...)...)
-	s.ideds = append(s.ideds[:i], append([]K{k}, s.ideds[i:]...)...)
-	//we did the equivalent of: sort.Sort(s)
-}
-
-func (s *dmap[K, V]) Len() int {
-	return len(s.keys)
-}
-func (s *dmap[K, V]) Swap(i, j int) {
-	s.keys[i], s.keys[j] = s.keys[j], s.keys[i]
-	s.vals[i], s.vals[j] = s.vals[j], s.vals[i]
-	s.ideds[i], s.ideds[j] = s.ideds[j], s.ideds[i]
-}
-func (s *dmap[K, V]) Less(i, j int) bool {
-	return s.keys[i] < s.keys[j]
+	// id already in tree, just update in place
+	prev := it.Item().(*ikv[K, V])
+	prev.key = key
+	prev.val = val
 }
 
 func all[K ided, V any](m *dmap[K, V]) iter.Seq2[K, V] {
 
 	return func(yield func(K, V) bool) {
-		for i := range m.keys {
-			if !yield(m.ideds[i], m.vals[i]) {
+
+		lim := m.tree.Limit()
+
+		for it := m.tree.Min(); it != lim; it = it.Next() {
+			kv := it.Item().(*ikv[K, V])
+			if !yield(kv.key, kv.val) {
 				return
 			}
 		}
