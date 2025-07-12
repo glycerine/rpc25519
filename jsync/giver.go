3c7d5b35a045e7de7a786c89152341657e87aeda
Jason E. Aten, Ph.D. <jason@devnull>
2025-07-12 20:19:58 +0200

    taker and giver logging back on

--- jsync/giver.go
+++ jsync/giver.go
@@ -90,7 +90,7 @@ func (s *SyncService) Giver(ctx0 context.Context, ckt *rpc.Circuit, myPeer *rpc.
 		select {
 
 		case frag0 := <-ckt.Reads:
-			//vv("%v: (ckt '%v') (Giver) saw read frag0:'%v'", name, ckt.Name, frag0)
+			vv("%v: (ckt '%v') (Giver) saw read frag0:'%v'", name, ckt.Name, frag0)
 
 			//vv("frag0 = '%v'", frag0)
 			switch frag0.FragOp {
