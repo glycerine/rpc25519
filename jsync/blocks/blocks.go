b186b2f4dbf55fd49e519c443963a43fd1bd857b
Jason E. Aten, Ph.D. <jason@devnull>
2025-07-22 20:40:38 +0200

    draft blocks.go

--- jsync/blocks/blocks.go
+++ jsync/blocks/blocks.go
@@ -1,37 +1,39 @@
 package block
 
 type ServerBlock struct {
-	VolumeID string
+	// where is encrypted data stored.
+	VolumeID string `zid:"0"`
+	Beg      int64  `zid:"1"` // where in volume does ServerData start.
+	Endx     int64  `zid:"2"` // Beg + len(ServerData)
 
-	Beg  int64 // where in volume does ServerData start.
-	Endx int64 // Beg + len(ServerData)
+	// encrypted data (nil unless sending over network).
+	ServerData []byte `zid:"3"`
 
-	ServerData []byte
-
-	// chucksum ServerData
-	ServerDataBlake3 string
+	// checksum ServerData
+	ServerDataBlake3 string `zid:"4"`
 }
 
 // client knows ClientBlock, and then
 // leaves ServerDataBlake3 as default empty string
 // when encrypting ClientBlock into ServerBlock.ServerData.
 type ClientBlock struct {
-	PrePad  []byte
-	Payload []byte
-	PostPad []byte
+	PayloadBlake3 string `zid:"0"`
 
 	// how DataPayload is compressed; empty string if none.
-	Compression   string
-	PayloadBlake3 string
+	Compression string `zid:"1"`
 
 	// generated before; will end up 1:1 with blake3 of this encrypted data.
-	WriteCallID string
-	WriteTime0  time.Time
-	Expires     time.Time // allow backups to be purged too.
-
-	VolumeID string
+	WriteCallID string    `zid:"2"`
+	WriteTime0  time.Time `zid:"3"`
+	Expires     time.Time `zid:"4"` // allow backups to be purged too.
 
 	// chucksum ServerData must be empty string when
 	// encrypting ClientBlock into ServerData.
-	ServerDataBlake3 string
+	// Then should be filled in so client knows
+	// how to request it back.
+	ServerDataBlake3 string `zid:"5"`
+
+	PrePad  []byte `zid:"6"` // cry-rand padding
+	Payload []byte `zid:"7"`
+	PostPad []byte `zid:"8"` // cry-rand padding
 }
