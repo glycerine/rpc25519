2ddf4c63a5b8c4a9e37d1a09512024d4bcb5e7dd
Jason E. Aten, Ph.D. <jason@devnull>
2025-11-22 01:15:34 -0600

    more debug prints on helper.go:189

--- tube/helper.go
+++ tube/helper.go
@@ -186,7 +186,7 @@ func (node *TubeNode) HelperFindLeader(cfg *TubeConfig, contactName string, requ
 	}
 
 	for xname, url := range xtra {
-		vv("on xtra name='%v', url='%v'", name, url)
+		vv("on xtra xname='%v', url='%v'", xname, url)
 		if xname == node.name {
 			continue // skip self
 		}
