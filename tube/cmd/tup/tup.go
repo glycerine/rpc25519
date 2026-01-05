ed4067c061ae7a96d183c589b179127b61674938
Jason E. Aten, Ph.D. <jason@devnull>
2026-01-05 04:39:15 -0300

    fix tup build

--- tube/cmd/tup/tup.go
+++ tube/cmd/tup/tup.go
@@ -431,7 +431,7 @@ repl:
 					oldval = "" // empty string
 				}
 				newval := args[3]
-				tkt, err := sess.CAS(ctx, tube.Key(table), tube.Key(key), tube.Val(oldval), tube.Val(newval), 0, "")
+				tkt, err := sess.CAS(ctx, tube.Key(table), tube.Key(key), tube.Val(oldval), tube.Val(newval), 0, "", leaseDur)
 				if err != nil {
 					fmt.Printf("error: %v\n", err)
 					sess = needNewSess(sess, err)
