ecec572285a2c922d8cbcf287bf4bafedb65c60b
Jason E. Aten, Ph.D. <jason@devnull>
2026-01-06 23:46:34 -0300

    member.go: draft in heartbeating to czar, but no czar receiving hb yet

--- tube/cmd/member/member.go
+++ tube/cmd/member/member.go
@@ -7,13 +7,14 @@ import (
 	"fmt"
 	//"io"
 	"os"
-	//"strings"
+	"strings"
 	//"path/filepath"
 	//"sort"
 	"time"
 
-	//rpc "github.com/glycerine/rpc25519"
+	rpc "github.com/glycerine/rpc25519"
 	//"github.com/glycerine/ipaddr"
+	"github.com/glycerine/idem"
 	"github.com/glycerine/rpc25519/tube"
 	//"github.com/glycerine/rpc25519/tube/art"
 )
@@ -37,6 +38,14 @@ func (c *ConfigMember) FinishConfig(fs *flag.FlagSet) (err error) {
 }
 
 func (c *ConfigMember) SetDefaults() {}
+
+type czarState int
+
+const (
+	unknownCzarState czarState = 0
+	amCzar           czarState = 1
+	notCzar          czarState = 2
+)
 
 func main() {
 	cmdCfg := &ConfigMember{}
@@ -81,56 +90,122 @@ func main() {
 	panicOn(err)
 	vv("got sess = '%v'", sess)
 
-	myDetail := cli.GetMyPeerDetail()
-
-	// find the czar. it might be me.
-	// we try to write to the "czar" key with a lease.
-	// first one there wins. everyone else reads the winner's URL.
-	list := cli.NewReliableMembershipList()
-	list.CzarName = cliName // if we win the write race, we are the czar.
-	list.PeerNames.Set(cliName, myDetail)
-	bts2, err := list.MarshalMsg(nil)
-	panicOn(err)
-
 	keyCz := "czar"
 	tableHermes := "hermes"
-	leaseDurCz := time.Minute
-	czarTkt, err := sess.Write(ctx, tube.Key(tableHermes), tube.Key(keyCz), tube.Val(bts2), 0, tube.ReliableMembershipListType, leaseDurCz)
-	_ = czarTkt
-
-	vers := tube.RMVersionTuple{
-		CzarLeaseEpoch: czarTkt.LeaseEpoch,
-		Version:        0,
-	}
-
 	var renewCzarLeaseCh <-chan time.Time
-	renewCzarLeaseDur := leaseDurCz / 2
-	if err == nil {
-		vv("err=nil on lease write. I am czar, send heartbeats to tube/raft to re-lease the hermes/czar key to maintain that status. vers = '%#v'", vers)
-		renewCzarLeaseCh = time.After(renewCzarLeaseDur)
-	} else {
-		if czarTkt.Vtype != tube.ReliableMembershipListType {
-			panicf("why not tube.ReliableMembershipListType back? got '%v'", czarTkt.Vtype)
-		}
-		_, err = list.UnmarshalMsg(czarTkt.Val)
-		panicOn(err)
 
-		vv("I am not czar, did not write to key: '%v'; vers='%#v'; list='%v'; \n with list.CzarName='%v'", err, vers, list, list.CzarName)
-		// contact the czar and register ourselves.
-		det, ok := list.PeerNames.Get2(list.CzarName)
-		if !ok {
-			panicf("list with winning czar did not include czar itself?? list='%v'", list)
-		}
-		vv("will contact czar '%v' at URL: '%v'", list.CzarName, det.URL)
-	}
+	leaseDurCz := time.Minute
+	renewCzarLeaseDur := leaseDurCz / 2
+
+	var cState czarState = unknownCzarState
+	var vers tube.RMVersionTuple
+	list := cli.NewReliableMembershipList()
+	myDetail := cli.GetMyPeerDetail()
+
+	var czarURL string
+	var czarCkt *rpc.Circuit
+
+	var memberHeartBeatCh <-chan time.Time
+	memberHeartBeatDur := time.Second * 10
+
+	halt := idem.NewHalter()
+	defer halt.Done.Close()
 
 	for {
-		select {
-		case <-renewCzarLeaseCh:
-			czarTkt, err = sess.Write(ctx, tube.Key(tableHermes), tube.Key(keyCz), tube.Val(bts2), 0, tube.ReliableMembershipListType, leaseDurCz)
+		switch cState {
+		case unknownCzarState:
+
+			// find the czar. it might be me.
+			// we try to write to the "czar" key with a lease.
+			// first one there wins. everyone else reads the winner's URL.
+			list.CzarName = cliName // if we win the write race, we are the czar.
+			list.PeerNames.Set(cliName, myDetail)
+			list.Vers = vers
+			bts2, err := list.MarshalMsg(nil)
 			panicOn(err)
-			vv("renewed czar lease, good until %v", nice(czarTkt.LeaseUntilTm))
-			renewCzarLeaseCh = time.After(renewCzarLeaseDur)
+
+			czarTkt, err := sess.Write(ctx, tube.Key(tableHermes), tube.Key(keyCz), tube.Val(bts2), 0, tube.ReliableMembershipListType, leaseDurCz)
+
+			if err == nil {
+				cState = amCzar
+				vers = tube.RMVersionTuple{
+					CzarLeaseEpoch: czarTkt.LeaseEpoch,
+					Version:        0,
+				}
+				vv("err=nil on lease write. I am czar, send heartbeats to tube/raft to re-lease the hermes/czar key to maintain that status. vers = '%#v'", vers)
+				renewCzarLeaseCh = time.After(renewCzarLeaseDur)
+			} else {
+				cState = notCzar
+				if czarTkt.Vtype != tube.ReliableMembershipListType {
+					panicf("why not tube.ReliableMembershipListType back? got '%v'", czarTkt.Vtype)
+				}
+				_, err = list.UnmarshalMsg(czarTkt.Val)
+				panicOn(err)
+
+				vv("I am not czar, did not write to key: '%v'; vers='%#v'; list='%v'; \n with list.CzarName='%v'", err, vers, list, list.CzarName)
+				// contact the czar and register ourselves.
+			}
+
+		case amCzar:
+			select {
+			case <-renewCzarLeaseCh:
+				list.Vers = vers
+				bts2, err := list.MarshalMsg(nil)
+				panicOn(err)
+
+				czarTkt, err := sess.Write(ctx, tube.Key(tableHermes), tube.Key(keyCz), tube.Val(bts2), 0, tube.ReliableMembershipListType, leaseDurCz)
+				panicOn(err)
+				vv("renewed czar lease, good until %v", nice(czarTkt.LeaseUntilTm))
+				renewCzarLeaseCh = time.After(renewCzarLeaseDur)
+			case <-halt.ReqStop.Chan:
+				return
+
+				// case handle request to register new member, and update
+				// the list that we periodically write to Tube/Raft.
+			}
+
+		case notCzar:
+			if czarCkt == nil {
+				czarDetail, ok := list.PeerNames.Get2(list.CzarName)
+				if !ok {
+					panicf("list with winning czar did not include czar itself?? list='%v'", list)
+				}
+				vv("will contact czar '%v' at URL: '%v'", list.CzarName, czarDetail.URL)
+				czarURL = czarDetail.URL
+
+				heartBeatFrag := cli.MyPeer.NewFragment()
+				heartBeatFrag.FragOp = tube.ReliableMemberHeartBeatToCzar
+				heartBeatFrag.FragSubject = "ReliableMemberHeartBeatToCzar"
+				heartBeatFrag.SetUserArg("URL", myDetail.URL)
+
+				czarCkt, _, _, err = cli.MyPeer.NewCircuitToPeerURL("member-to-czar", czarURL, heartBeatFrag, 0)
+				panicOn(err)
+				vv("got circuit to czar: %v", czarCkt)
+				cli.MyPeer.NewCircuitCh <- czarCkt // needed/desirable?
+
+				memberHeartBeatCh = time.After(memberHeartBeatDur)
+			}
+			select {
+			case <-memberHeartBeatCh:
+				heartBeatFrag := cli.MyPeer.NewFragment()
+				heartBeatFrag.FragOp = tube.ReliableMemberHeartBeatToCzar
+				heartBeatFrag.FragSubject = "ReliableMemberHeartBeatToCzar"
+				heartBeatFrag.SetUserArg("URL", myDetail.URL)
+
+				err = cli.SendOneWay(czarCkt, heartBeatFrag, 0, 0)
+
+				const connRefused = "connect: connection refused"
+				if strings.Contains(err.Error(), connRefused) {
+					vv("connection refused to (old?) czar, transition to unknownCzarState and write/elect a new czar")
+					czarCkt = nil
+					cState = unknownCzarState
+					continue
+				}
+				memberHeartBeatCh = time.After(memberHeartBeatDur)
+
+			case <-halt.ReqStop.Chan:
+				return
+			}
 		}
 	}
 }
