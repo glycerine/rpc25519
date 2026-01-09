f8a4a8bcbffdb3a51b986b60a346b5a7b947bc36
Jason E. Aten, Ph.D. <jason@devnull>
2026-01-08 22:42:00 -0300

    sketch hlc_test.go

---
+++ tube/hlc_test.go
@@ -0,0 +1,14 @@
+package tube
+
+import (
+	"testing"
+)
+
+// functions to test
+// func PhysicalTime48() HLC
+// func (j *HLC) CreateSendOrLocalEvent()
+// func (j *HLC) ReceiveMessageWithHLC(m HLC)
+// func (a HLC) GT(b HLC) bool
+// func (a HLC) GTE(b HLC) bool
+// func (a HLC) LT(b HLC) bool
+// func (a HLC) LTE(b HLC) bool
