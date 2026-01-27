e7dd74218e8a312ca1a8b158968296f27e968741
Jason E. Aten <j.e.aten@gmail.com>
2026-01-27 14:31:22 -0600

    atgs. comment on limitlsn origin/license

--- limitlsn.go
+++ limitlsn.go
@@ -3,6 +3,9 @@ package rpc25519
 // version of golang.org/x/net/netutil#LimitListener
 // that supports tls.Conn
 
+// This file derived from golang.org/x/net/netutil/listen.go
+// which uses the same license as our LICENSE, so the license
+// reporting obligation is met:
 // Copyright 2013 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
