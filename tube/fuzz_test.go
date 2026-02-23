8a2f0cec3e9a9024e265a6cd64e07ed887a83295
Jason E. Aten, Ph.D. <jason@devnull>
2026-02-23 15:07:03 -0300

    atgs. fuzz_test show env var at top

--- tube/fuzz_test.go
+++ tube/fuzz_test.go
@@ -1873,6 +1873,7 @@ func Test101_userFuzz(t *testing.T) {
 	// for batch runs from env var partition.
 	beg := os.Getenv("SCEN0")
 	batchN := os.Getenv("SCEN_BATCH")
+	vv("SCEN0 = '%v' ; SCEN_BATCH = '%v'", beg, batchN)
 	if beg != "" && batchN != "" {
 		var err error
 		begScenario, err = strconv.Atoi(beg)
