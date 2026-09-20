cd8c11af111035ed69b8998fa4d3465fb9aae764
Jason E. Aten <j.e.aten@gmail.com>
2026-09-20 16:29:57 -0500

    atg. note one ckt_test.go flaky 409 under pont and synctest, very rarely

--- ckt_test.go
+++ ckt_test.go
@@ -746,7 +746,7 @@ func Test409_lots_of_send_and_read(t *testing.T) {
 		t.Fatalf("error: expected srv readch to have %v, got: %v", want, got)
 	}
 	if got, want := len(j.clis.sendch), 2+N; got != want {
-		t.Fatalf("error: expected cli sendch to have %v, got: %v", want, got)
+		t.Fatalf("error: expected cli sendch to have %v, got: %v", want, got) // intermittant, very rare: ckt_test.go:749: error: expected cli sendch to have 12, got: 11 ; under go test -v -tags synctest -tags pont ; pont as of 2026-Sept-20.
 	}
 	if got, want := len(j.srvs.sendch), 1+N; got != want {
 		t.Fatalf("error: expected srv sendch to have %v, got: %v", want, got)
